9c939fc685fd746c6bf48d19abc5afa524f95fcb
Gaurav Kumar <gkumar@tcd1.com>
2023-08-15 01:06:14 +0530

    dev

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
     devServer: {
       proxy: {
         '/dev/**': {
-          target:'https://f8ilxp81c1-vpce-07c1e4021ba74fe08.execute-api.us-east-1.amazonaws.com',
+          target:'',
           changeOrigin: true
         }
       }
